3b06b14ad11ee2924291e2bb96d950ad4a0dc2a9
Victor Omoniyi <vomoniyi2001@gmail.com>
2022-03-15 07:39:33 +0100

    binary search solution

---
+++ Arrays/704. Binary Search/704. Binary Search.py
@@ -0,0 +1,16 @@
+from pip import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l,r = 0, len(nums)-1
+        while l <= r:
+            mid = l + (r-l) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return -1
+        
