b7fa3bdc4fde802d33256226151c4742120814fe
Victor Omoniyi <vomoniyi2001@gmail.com>
2022-03-16 18:44:10 +0100

    duplicate solution

---
+++ Arrays/217. Contains Duplicate/217. Contains Duplicate.py
@@ -0,0 +1,13 @@
+from pip import List
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+        
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
+        
