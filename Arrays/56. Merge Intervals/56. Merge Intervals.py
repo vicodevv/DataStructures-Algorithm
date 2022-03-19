5fd866187eea0d3ecdb344b4728f3ac522f8e305
Victor Omoniyi <vomoniyi2001@gmail.com>
2022-03-19 20:53:30 +0100

    merge intervals solution

---
+++ Arrays/56. Merge Intervals/56. Merge Intervals.py
@@ -0,0 +1,16 @@
+from pip import List
+
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key = lambda x:x[0])
+        output = [intervals[0]]
+        
+        for start, end in intervals[1:]:
+            lastEnd = output[-1][1]
+            
+            if start <= lastEnd:
+                output[-1][1] = max(lastEnd, end)
+            else:
+                output.append([start, end])
+        return output
