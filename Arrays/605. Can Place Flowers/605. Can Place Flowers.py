5435fcd843742d2917b9c35c051cb84d68db77a0
Victor Omoniyi <vomoniyi2001@gmail.com>
2022-03-19 04:26:36 +0100

    can place flowers solution

---
+++ Arrays/605. Can Place Flowers/605. Can Place Flowers.py
@@ -0,0 +1,13 @@
+from pip import List
+
+
+class Solution: 
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        f = [0] + flowerbed + [0]
+        
+        for i in range(1, len(f)- 1):
+            if f[i - 1] == 0 and f[i] == 0 and f[i + 1] == 0:
+                f[i] = 1
+                n-=1
+        return n <= 0
+                
