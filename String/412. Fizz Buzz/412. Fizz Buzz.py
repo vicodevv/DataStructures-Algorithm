895794525632d84077788c9dfc5b81d5895e73e1
Victor Omoniyi <vomoniyi2001@gmail.com>
2022-03-22 01:24:09 +0100

    fizz buzz solution

---
+++ String/412. Fizz Buzz/412. Fizz Buzz.py
@@ -0,0 +1,16 @@
+from pip import List
+
+
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        res = []
+        for i in range(1, n+1):
+            if i % 15 == 0:
+                res.append("FizzBuzz")
+            elif i % 3 == 0:
+                res.append ("Fizz")
+            elif i % 5 == 0:
+                res.append("Buzz")
+            else:
+                res.append(str(i))
+        return res
