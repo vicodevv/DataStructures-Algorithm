1f26e5fe3f07336a02ab2298c964d29ea4d40dbd
Victor Omoniyi <vomoniyi2001@gmail.com>
2022-03-29 14:01:04 +0100

    median of two sorted arrays solution

---
+++ Arrays/4. Median of Two Sorted Arrays/4. Median of Two Sorted Arrays.py
@@ -0,0 +1,44 @@
+from pip import List
+
+#Easy solution
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        sum = nums1 + nums2
+        sum = sorted(sum)
+        l = len(sum)
+        if l%2 == 0:
+            n = l // 2
+            median = (sum[n]+sum[n-1]) / 2
+        else:
+            n = l // 2
+            median = sum[n]
+        return median
+
+#Hard solution
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        A, B = nums1, nums2
+        total = len(nums1) + len(nums2)
+        mid = total // 2
+        
+        if len(B) < len(A):
+            A, B = B, A
+        
+        l, r = 0, len(A) - 1
+        while True:
+            i = (l+r)//2
+            j = mid-i-2
+        
+            Aleft = A[i] if i >= 0 else float("-infinity")
+            Aright = A[i + 1] if (i + 1)< len(A) else float("infinity")
+            Bleft = B[j] if j >= 0 else float("-infinity")
+            Bright = B[j + 1] if (j + 1)< len(B) else float("infinity")
+            
+            if Aleft <= Bright and Bleft <= Aright:
+                if total%2:
+                    return min(Aright, Bright)
+                return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
+            elif Aleft > Bright:
+                r = i - 1
+            else:
+                l = i + 1
