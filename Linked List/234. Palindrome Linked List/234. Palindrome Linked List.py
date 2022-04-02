14465e1bdfca8409a7af686dc4d148f56d54e3de
Victor Omoniyi <vomoniyi2001@gmail.com>
2022-04-02 01:57:17 +0100

    palindrom linked list solution

--- Linked List/234. Palindrome Linked List/234. Palindrome Linked List.py
+++ Linked List/234. Palindrome Linked List/234. Palindrome Linked List.py
@@ -6,7 +6,37 @@
 from tkinter.tix import ListNoteBook
 from typing import Optional
 
+def reverse(current,prev):
+    if current == None:
+        return prev
+    next_node = current.next
+    current.next = prev
+    return reverse(next_node,current)
+    
+    
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        if head.next == None:
+            return True
+        s,f,prev = head,head,None
+        while f and f.next:
+            prev=s
+            s= s.next
+            f= f.next.next
+            
+        prev.next=None
+        head1 = head
+        head2 = s if f==None else s.next
+        head2 = reverse(head2,None)
+        while head2 and head1:
+            if head1.val!=head2.val:
+                return False
+            head1=head1.next
+            head2=head2.next
+        return True
 
+
+#This second solution would require extra memory since we're converting the list to an array
 class Solution:
     def isPalindrome(self, head: Optional[ListNoteBook]) -> bool:
         nums = []
