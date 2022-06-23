179d4d974326949b66887fc058b643e645d9af26
Victor Omoniyi <vomoniyi2001@gmail.com>
2022-06-23 15:31:56 +0100

    design a leaderboard solution

---
+++ Design/1244. Design A Leaderboard/1244. Design A Leaderboard.py
@@ -0,0 +1,21 @@
+from collections import Counter
+class Leaderboard:
+
+    def __init__(self):
+        self.counter = Counter()
+
+    def addScore(self, playerId: int, score: int) -> None:
+        self.counter[playerId] += score
+
+    def top(self, K: int) -> int:
+        return sum(map(lambda x:x[1], self.counter.most_common(K)))
+
+    def reset(self, playerId: int) -> None:
+        self.counter[playerId] = 0
+
+
+# Your Leaderboard object will be instantiated and called as such:
+# obj = Leaderboard()
+# obj.addScore(playerId,score)
+# param_2 = obj.top(K)
+# obj.reset(playerId)
